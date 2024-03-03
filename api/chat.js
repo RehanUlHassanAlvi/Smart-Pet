4cf7e6c674936b73666a50c96530b3d7b3c7b0c0
RehanUlHassanAlvi <rehan_ulhassan@yahoo.com>
2024-03-03 22:50:03 +0500

    get list of user

--- api/chat.js
+++ api/chat.js
@@ -27,7 +27,7 @@ router.post('/messages', async (req, res) => {
       await chatHistory.save();
   
       // Trigger 'message' event on 'chat' channel (Pusher)
-      pusher.trigger('chat', 'message', { sender, text });
+      pusher.trigger('chat', 'message', { sender, text ,userId});
   
       res.sendStatus(200);
     } catch (error) {
